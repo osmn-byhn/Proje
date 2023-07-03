cbcedef4e025dab5d6451857d04e83ce46621c95
osmn-byhn <osmanbeyhan12@gmail.com>
2023-07-03 06:20:58 +0300

    Commit

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 3000
+
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+const port = 3000
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- client/src/views/guestView/HomeView.vue
+++ client/src/views/guestView/HomeView.vue
@@ -1,5 +1,43 @@
 <script setup>
     import Navbar from '../../components/guestComponent/Navbar.vue';
+    import axios from 'axios'
+
+    // Trendyol API kimlik bilgilerini burada güncelleyin
+    const apiKey = 'FlZAAQEYFTstE0WwTq91';
+    const apiSecret = 'XlLYXiuXRa4mpkxoNoJ9';
+    const clientId = '817589';
+
+    // Trendyol API erişim URL'ini burada güncelleyin
+    const trendyolApiUrl = 'https://api.trendyol.com';
+
+    const authorize = async () => {
+    try {
+        const response = await axios.post(`${trendyolApiUrl}/authorization/v2/oauth/token`, null, {
+        params: {
+            grant_type: 'client_credentials',
+            client_id: clientId,
+            client_secret: apiSecret,
+        },
+        });
+
+        const accessToken = response.data.access_token;
+        console.log('Access Token:', accessToken);
+        return accessToken;
+    } catch (error) {
+        console.error('Yetkilendirme hatası:', error.response.data);
+        throw error;
+    }
+    };
+
+    authorize()
+    .then((accessToken) => {
+        // Access Token'ı kullanarak diğer Trendyol API isteklerini gerçekleştirin
+        console.log('sonunda oldu amk');
+        // ...
+    })
+    .catch((error) => console.error('Hata:', error));
+
+
 </script>
 
 <template>
